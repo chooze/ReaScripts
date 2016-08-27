aae9dc377dddc533f7345e0be034fa04c4aadd9e
Alexey Chooze <leshachooze@gmail.com>
2016-08-27 06:22:30 +0300

    Add files via upload

---
+++ Items/Chooze_Split and half.lua
@@ -0,0 +1,27 @@
+--[[
+   * ReaScript Name: Split and half
+   * Lua script for Cockos REAPER
+   * Author: Chooze
+   * Author URI: http://vk.com/podushampublic http://choozeyoursound.com
+   * Licence: GPL v3
+   * Version: 1.3
+  ]]
+
+
+scriptname = "Split and half"
+
+
+reaper.Undo_BeginBlock()
+
+reaper.Main_OnCommand("40932", 0)
+
+  Item_No = reaper.CountSelectedMediaItems(0)
+  
+    for i = 0, Item_No - 1 do
+      Item = reaper.GetSelectedMediaItem(0, i)
+      Item_Len = reaper.GetMediaItemInfo_Value(Item, "D_LENGTH") 
+      Item_Len_Half = reaper.SetMediaItemLength(Item, Item_Len / 2, 0)
+    end
+  reaper.UpdateArrange()
+  
+reaper.Undo_EndBlock(scriptname, 0)
